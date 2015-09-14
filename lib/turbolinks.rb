af3d7bddfd3362054eadae76560963907eecaf63
Sam Stephenson <sam@37signals.com>
2015-09-14 16:58:41 -0500

    Implement Turbolinks redirection
    Replaces the current history entry by default; call `redirect_to(..., turbolinks: :advance)` to push a new history entry.
    
    Cache is cleared for redirections from non-GET requests.

--- lib/turbolinks.rb
+++ lib/turbolinks.rb
@@ -1,8 +1,21 @@
 require 'turbolinks/version'
+require 'turbolinks/redirection'
 
 module Turbolinks
+  module Controller
+    include Redirection
+  end
+
   class Engine < ::Rails::Engine
+    config.turbolinks = ActiveSupport::OrderedOptions.new
+    config.turbolinks.auto_include = true
+
     initializer :turbolinks do |app|
+      ActiveSupport.on_load(:action_controller) do
+        if app.config.turbolinks.auto_include
+          include Controller
+        end
+      end
     end
   end
 end
---
+++ lib/turbolinks/redirection.rb
@@ -0,0 +1,24 @@
+module Turbolinks
+  module Redirection
+    def redirect_to(url = {}, options = {})
+      turbolinks = options.delete(:turbolinks)
+      value = super(url, options)
+
+      if turbolinks || (turbolinks != false && request.xhr? && (options.size > 0 || !request.get?))
+        visit_options = {
+          action: turbolinks.to_s == "advance" ? turbolinks : "replace"
+        }
+
+        script = []
+        script << "Turbolinks.clearCache()" unless request.get?
+        script << "Turbolinks.visit(#{location.to_json}, #{visit_options.to_json})"
+
+        self.status = 200
+        self.response_body = script.join("\n")
+        response.content_type = Mime::JS
+      end
+
+      value
+    end
+  end
+end

a3e943d1b27c04da91679c4867fe278e5a6e7394
Jeffrey Hardy <packagethief@gmail.com>
2015-09-17 15:09:11 -0400

    Use super.tap to preserve the original return value

--- lib/turbolinks/redirection.rb
+++ lib/turbolinks/redirection.rb
@@ -2,23 +2,22 @@ module Turbolinks
   module Redirection
     def redirect_to(url = {}, options = {})
       turbolinks = options.delete(:turbolinks)
-      value = super(url, options)
+      
+      super(url, options).tap do
+        if turbolinks != false && request.xhr? && !request.get?
+          visit_options = {
+            action: turbolinks.to_s == "advance" ? turbolinks : "replace"
+          }
 
-      if turbolinks != false && request.xhr? && !request.get?
-        visit_options = {
-          action: turbolinks.to_s == "advance" ? turbolinks : "replace"
-        }
+          script = []
+          script << "Turbolinks.clearCache()" unless request.get?
+          script << "Turbolinks.visit(#{location.to_json}, #{visit_options.to_json})"
 
-        script = []
-        script << "Turbolinks.clearCache()" unless request.get?
-        script << "Turbolinks.visit(#{location.to_json}, #{visit_options.to_json})"
-
-        self.status = 200
-        self.response_body = script.join("\n")
-        response.content_type = Mime::JS
+          self.status = 200
+          self.response_body = script.join("\n")
+          response.content_type = Mime::JS
+        end
       end
-
-      value
     end
   end
 end
